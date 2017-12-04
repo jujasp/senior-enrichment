fdb9b0b4d9e4c0fd10aae5beec07e6a751d2c756
Julia Jaspers <juliajaspers@Julias-MBP.home>
2017-12-03 20:33:49 -0500

    campuses '/' get working

--- server/api/campuses.js
+++ server/api/campuses.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const models = require('../../db/models');
+const models = require('../db/models');
 const Campus = models.Campus;
 
 module.exports = router;
--- server/api/students.js
+++ server/api/students.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const models = require('../../db/models');
+const models = require('../db/models');
 const Student = models.Student;
 
 module.exports = router;
